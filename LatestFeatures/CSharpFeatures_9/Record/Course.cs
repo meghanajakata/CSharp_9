9208351f8f8823cdf8c8c3c5b04365c894f44c17
meghanajakata <jakatameghana@gmail.com>
2023-12-13 19:50:51 +0530

    Modified Code

---
+++ LatestFeatures/CSharpFeatures_9/Record/Course.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_9.Record
+{
+    public record Course
+    {
+        public string Title1 { get; set; }
+        public string Category { get; set; }
+        public int Price { get; set; }
+        public bool isreadon { get; set; }
+
+    }
+
+    public class CourseMaterials
+    {
+        public string Title { get; set; }
+        public string Category { get; set; }
+        public int Price { get; set; }
+    }
+}
